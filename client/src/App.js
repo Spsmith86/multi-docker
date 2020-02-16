fdb803ca736c7bf3aaaec02e0f41b2ee0f74eaef
Sean Smith <ssmith468@gatech.edu>
2020-02-16 09:25:32 -0800

    Added title

--- client/src/App.js
+++ client/src/App.js
@@ -11,20 +11,13 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Fib Calculator</h1>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
           <div>
             <Route exact path="/" component={Fib} />
             <Route path="/otherpage" component={OtherPage} />
           </div>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     </Router>
